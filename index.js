0bbda19dc19fe7ee09632c696558a2ff5bb8dc1c
samuraizor <andrewzinho@gmail.com>
2019-02-05 08:19:08 +0000

    cookie2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var cardlist = "";
 var cardArr = [];
 var deckCont = 0
 if (getCookie("deckstring")!=""){
-    $("#string").val(getCookie("deckstring"));
+    $("#string").html(getCookie("deckstring"));
 }
 
 
@@ -52,7 +52,7 @@ function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+s    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     console.log(cname + "=" + cvalue + ";" + expires + ";path=/")
   }
   
